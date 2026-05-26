f0a5aa0706f19f59198bd32e5b9248801ee0d690
sjj050121014 <sjj050121014@163.com>
2026-05-27 00:09:56 +0800

    Remove redundant texture parameter settings in GLLightingMaps::initializeGL

--- src/gllightingmaps.cpp
+++ src/gllightingmaps.cpp
@@ -125,11 +125,6 @@ void GLLightingMaps::initializeGL()
 
     // Load diffuse map
     m_diffuseMap = loadTexture(":/textures/container2.png");
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 }
 
 void GLLightingMaps::paintGL()
