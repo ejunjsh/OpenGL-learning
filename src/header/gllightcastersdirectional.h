d1984bf58987a993bff19c011e855ef274f7221a
sjj050121014 <sjj050121014@163.com>
2026-05-27 19:30:35 +0800

    Add GLLightCastersDirectional class with vertex and fragment shaders

--- src/header/gllightcastersdirectional.h
+++ src/header/gllightcastersdirectional.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "glcamerabase.h"
+#include <QOpenGLShaderProgram>
 
 class GLLightCastersDirectional : public GLCameraBase
 {
@@ -11,4 +12,12 @@ public:
 protected:
     void initializeGL() override;
     void paintGL() override;
+
+private:
+    GLuint m_cubeVAO = 0;
+    GLuint m_vbo = 0;
+    GLuint m_diffuseMap = 0;
+    GLuint m_specularMap = 0;
+
+    QOpenGLShaderProgram m_program;
 };
